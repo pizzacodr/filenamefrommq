3daf9a938046aacabef5c5e94bb7c30426df918f
pizzacodr <pizzacodr@users.noreply.github.com>
2019-07-06 20:25:39 -0400

    initial commit

---
+++ src/main/java/com/github/pizzacodr/filenamefrommq/App.java
@@ -0,0 +1,13 @@
+package com.github.pizzacodr.filenamefrommq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
