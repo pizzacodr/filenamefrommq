30fc49ca0ed128ffd407beb3583ac2cf1da0e023
pizzacodr <pizzacodr@users.noreply.github.com>
2019-07-09 23:24:53 -0400

    Initial work on reading from the queue

--- src/main/java/com/github/pizzacodr/filenamefrommq/App.java
+++ src/main/java/com/github/pizzacodr/filenamefrommq/App.java
@@ -1,13 +1,56 @@
 package com.github.pizzacodr.filenamefrommq;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.DeliverCallback;
+import com.rabbitmq.client.Delivery;
+
+public class App {
+
+	private static ConnectionFactory factory;
+
+	public static void main(String[] args) throws IOException, TimeoutException {
+		Logger logger = LoggerFactory.getLogger(App.class);
+
+		String hostname = "localhost";
+		String queueName = "filename";
+
+		setupConnectionFactory(hostname, logger);
+
+		try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
+
+			channel.queueDeclare(queueName, false, false, false, null);
+			logger.info("Queue Name: " + queueName);
+
+			channel.basicQos(1); // maximum number of files the server will deliver before an ack.
+
+			//DefaultConsumer consumer = new DefaultConsumer(channel);
+			//channel.basicConsume(queueName, false, consumer); // no automatic ack
+
+			DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+				String message = new String(delivery.getBody(), "UTF-8");
+
+				logger.info(" [x] Received '" + message + "'");
+
+				logger.info(" [x] Done");
+				channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+			};
+			channel.basicConsume(queueName, false, deliverCallback, consumerTag -> {});
+		}
+	}
+
+	private static void setupConnectionFactory(String hostname, Logger logger) {
+		factory = new ConnectionFactory();
+		factory.setHost(hostname);
+
+		logger.info("Hostname: " + hostname);
+	}
 }
