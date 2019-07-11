bfdfc7578b5394c54116266ded828456a3716f9c
pizzacodr <pizzacodr@users.noreply.github.com>
2019-07-11 08:41:55 -0400

    exploring the options with RabbitMQ

--- src/main/java/com/github/pizzacodr/filenamefrommq/App.java
+++ src/main/java/com/github/pizzacodr/filenamefrommq/App.java
@@ -12,6 +12,7 @@ import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.DeliverCallback;
 import com.rabbitmq.client.Delivery;
+import com.rabbitmq.client.GetResponse;
 
 public class App {
 
@@ -27,23 +28,39 @@ public class App {
 
 		try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
 
-			channel.queueDeclare(queueName, false, false, false, null);
+			//channel.queueDeclare(queueName, false, false, false, null);
 			logger.info("Queue Name: " + queueName);
 
-			channel.basicQos(1); // maximum number of files the server will deliver before an ack.
+			//channel.basicQos(1); // maximum number of files the server will deliver before an ack.
 
 			//DefaultConsumer consumer = new DefaultConsumer(channel);
-			//channel.basicConsume(queueName, false, consumer); // no automatic ack
+			//channel.basicConsume(queueName, false); // no automatic ack
+			GetResponse response = channel.basicGet(queueName, false);
+			
+			logger.info(Integer.toString(response.getMessageCount()));
+			
+			while (response != null) {
+				logger.info(response.getBody().toString());
+				channel.basicAck(response.getEnvelope().getDeliveryTag(), false);
+			}
+			
+			/*
+			 * logger.info("consumers: " + channel.consumerCount(queueName) + " Tag: " +
+			 * consumer.getConsumerTag());
+			 */
+			
+			
 
-			DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-				String message = new String(delivery.getBody(), "UTF-8");
-
-				logger.info(" [x] Received '" + message + "'");
-
-				logger.info(" [x] Done");
-				channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
-			};
-			channel.basicConsume(queueName, false, deliverCallback, consumerTag -> {});
+			/*
+			 * DeliverCallback deliverCallback = (consumerTag, delivery) -> { String message
+			 * = new String(delivery.getBody(), "UTF-8");
+			 * 
+			 * logger.info(" [x] Received '" + message + "'");
+			 * 
+			 * logger.info(" [x] Done");
+			 * channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false); };
+			 * channel.basicConsume(queueName, false, deliverCallback, consumerTag -> {});
+			 */
 		}
 	}
 
