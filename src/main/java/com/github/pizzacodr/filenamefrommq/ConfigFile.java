8927892525f1e84b2150f0cea05fd868ca4be70d
pizzacodr <pizzacodr@users.noreply.github.com>
2019-07-13 21:09:49 -0400

    Added properties class

---
+++ src/main/java/com/github/pizzacodr/filenamefrommq/ConfigFile.java
@@ -0,0 +1,22 @@
+package com.github.pizzacodr.filenamefrommq;
+
+import org.aeonbits.owner.Config;
+import org.aeonbits.owner.Config.Sources;
+
+@Sources({ "file:${user.dir}/configFilenameFromMQ.properties", 
+    "file:${user.home}/configFilenameFromMQ.properties"})
+
+public interface ConfigFile extends Config {
+	
+	@DefaultValue("${user.home}/watchDir")
+	String watchDir();
+	
+	@DefaultValue("localhost")
+	String hostname();
+	
+	@DefaultValue("filename")
+	String queueName();
+	
+	@DefaultValue("2")
+	int waitTime();
+}
